e5dc2f132facccacb0c9cece93e13b7e320c6197
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2021-09-06 21:16:48 +0600

    project setup and style project

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    name: "Samsung Galaxy M02s",
+    price: 12000,
+    img: "https://static-01.daraz.com.bd/p/mdc/1887e7da2a4a8b2bf9faec0469782c38.jpg",
+  },
+  {
+    name: "Samsung Galaxy M02s",
+    price: 12000,
+    img: "https://static-01.daraz.com.bd/p/mdc/88926bb91e0148914a509d25169ac1df.jpg",
+  },
+  {
+    name: "Samsung Galaxy M02s",
+    price: 12000,
+    img: "https://static-01.daraz.com.bd/p/mdc/df018c4086e51ae78760cc4cd3b468b8.jpg",
+  },
+  {
+    name: "Samsung Galaxy M02s",
+    price: 12000,
+    img: "https://static-01.daraz.com.bd/p/767586c589d2f6f396344cf886fe0e7a.jpg",
+  },
+  {
+    name: "Samsung Galaxy M02s",
+    price: 12000,
+    img: "https://static-01.daraz.com.bd/p/b9eece7c4715c25c294169b5eaf4317c.jpg",
+  },
+];
